61c26f8fae1a79fcd7c9799da3078cff52ec414e
Pedro <pedro.dacruz@dcomp.ufs.br>
2025-04-29 05:04:57 -0300

    :dizzy: feat:Animações e reset de morte adicionados

--- Assets/Scripts/Goomba.cs
+++ Assets/Scripts/Goomba.cs
@@ -7,10 +7,20 @@ public class Goomba : MonoBehaviour
     [SerializeField] Transform p1;
     [SerializeField] LayerMask canoLayer; 
     [SerializeField] float distancia = 0.2f;
-
+    Animator MorteG,GoombaWalk;
+    BoxCollider2D ColisorG;
+    [SerializeField]bool MortoM = false;
     void Awake()
     {
         rbGoomba = GetComponent<Rigidbody2D>();
+        MorteG = GetComponent<Animator>();
+        ColisorG = GetComponent<BoxCollider2D>();
+        GoombaWalk = GetComponent<Animator>();
+    }
+        private void Start()
+    {
+        MortoM=false;
+
     }
 
     private void FixedUpdate()
@@ -25,5 +35,31 @@ public class Goomba : MonoBehaviour
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
             velocidade *= -1;
         }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag =="Player"){
+            if(transform.position.y+0.5f<collision.transform.position.y && MortoM==false)
+        {   collision.GetComponent<Rigidbody2D>().linearVelocity=Vector2.zero;
+            collision.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 6, ForceMode2D.Impulse);
+            MorteG.SetTrigger("MorteG");
+            velocidade=0;
+            Destroy(gameObject,0.3f); 
+            ColisorG.enabled=false;
+        }
+            else {
+            MortoM=true;
+            ColisorG.enabled=false;
+            FindFirstObjectByType<Movimentacao1>().Morte();
+            Goomba[] goomba = FindObjectsByType<Goomba>(FindObjectsSortMode.None);
+            //faz um for e enche todos os goombas com velocidade 0 paradinhos pra eles nao se mexerem quando tu morre
+            for(int j = 0;j<goomba.Length;j++){
+                goomba[j].velocidade=0;
+                goomba[j].GoombaWalk.speed = 0;
+            }
+        }
+
+    }
+
     }
 }
--- Assets/Scripts/Movimentacao1.cs
+++ Assets/Scripts/Movimentacao1.cs
@@ -1,5 +1,6 @@
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movimentacao1 : MonoBehaviour
 {
@@ -8,27 +9,35 @@ public class Movimentacao1 : MonoBehaviour
     [SerializeField] float forcaPulo = 8f;
     [SerializeField] bool pulando;
     [SerializeField] bool Chao = true;
-    [SerializeField] bool Cano = true;
-
     [SerializeField] Transform chaoCheck;
     [SerializeField] LayerMask chaoLayer;
 
-    Animator animPlayer;
-    Animator animPPulo;
+    Animator animPlayer,animPPulo,animMorte;
+    [SerializeField]bool MortoM = false;
+    BoxCollider2D MarioColisor;
+    bool MortoM1;
 
     private void Awake()
     {
         rbMario = GetComponent<Rigidbody2D>();
         animPlayer = GetComponent<Animator>();
         animPPulo = GetComponent<Animator>();
+        animMorte = GetComponent<Animator>();
+        MarioColisor = GetComponent<BoxCollider2D>();
+    }
+    private void Start()
+    {
+        MortoM=false;
+
     }
 
     private void Update()
     {
+        if (MortoM) return;
+
         Chao = Physics2D.Linecast(transform.position, chaoCheck.position, chaoLayer);
         Debug.DrawLine(transform.position, chaoCheck.position, Color.blue);    
         animPPulo.SetBool("pulando",!Chao);
-
 
         if (Input.GetButtonDown("Jump") && Chao){
             pulando = true;
@@ -47,20 +56,49 @@ public class Movimentacao1 : MonoBehaviour
 
     void Mover()
     {
-        float x = Input.GetAxis("Horizontal");
-        rbMario.linearVelocity = new Vector2(x * vel, rbMario.linearVelocity.y);
-        animPlayer.SetFloat("velocidade",Mathf.Abs(x));
-        if(x<0){
-            transform.eulerAngles = new Vector2(0,180);
-        }
-        else if(x>0) transform.eulerAngles = new Vector2(0,0);
-
-    }
+        if(MortoM) return;
+ 
+            float x = Input.GetAxis("Horizontal");
+            rbMario.linearVelocity = new Vector2(x * vel, rbMario.linearVelocity.y);
+            animPlayer.SetFloat("velocidade",Mathf.Abs(x));
+            if(x<0){
+                transform.eulerAngles = new Vector2(0,180);
+            }
+            else if(x>0) transform.eulerAngles = new Vector2(0,0);
+            }
+        
     void MarioPula(){
-        if (pulando){
-            rbMario.linearVelocity = Vector2.up * forcaPulo;
-            pulando = false;
-        }
-    
+        if(MortoM)return;
+
+            if (pulando){
+                rbMario.linearVelocity = Vector2.up * forcaPulo;
+                pulando = false;
+            }
+        
     }
+    public void Morte(){
+        StartCoroutine(MorteCoroutine());
+    }
+
+        IEnumerator MorteCoroutine(){
+    if(!MortoM){
+        //Fazendo ele ficar com a animacao certa :3
+        animMorte.SetTrigger("MorteM");
+        MortoM=true;
+        yield return new WaitForSeconds(0.5f);
+        //Fazeno o caba ficar parado
+        rbMario.linearVelocity = Vector2.zero;
+        //Paizao aí tu ta fzendo o caba ficar invisivel/intangivel
+        MarioColisor.isTrigger=true;
+        //Olha o pulinho do pai
+        rbMario.AddForce(Vector2.up*15f,ForceMode2D.Impulse);
+        Invoke("Reinicia",2.5f);
+    }
+    
+
+}
+    void Reinicia(){
+        SceneManager.LoadScene("Inicio");
+    }
+
 }
